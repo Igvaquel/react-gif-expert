b78acc9bbec2d45b6238a132c49143fec18bda03
ignacio vaquel <ignaciovaquel182@gmail.com>
2023-09-12 18:43:25 -0300

    Firs commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [inputValue, setInputValue] = useState("");
+
+    const onInputChange = ( {target } ) => {
+        setInputValue( target.value )
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const newCategory = inputValue.trim()
+        if( newCategory.length <= 1) return;
+
+        //setCategories( categories => [inputValue, ...categories]);
+        onNewCategory( newCategory );
+        setInputValue('');
+    }
+
+  return (
+    <form onSubmit={ ( event ) => onSubmit(event) }>
+        <input
+            type="text"
+            placeholder="Buscar gifs"
+            value={ inputValue }
+            onChange={ onInputChange }
+        />
+    </form>
+  )
+}
